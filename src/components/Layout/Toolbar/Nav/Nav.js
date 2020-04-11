eee6ae2fc0cda6d5f427361e015227e02f9d0f88
Nurzada <nura.okenova@gmail.com>
2020-04-11 11:16:07 +0600

    New components Logo , Nav

---
+++ src/components/Layout/Toolbar/Nav/Nav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from "./Nav.module.css";
+export default () => (
+<Nav className={classes.Nav}>
+    Nav
+</Nav>
+)
--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import classes from "./Toolbar.module.css"
+import Logo from "./Logo/Logo";
+import Nav from './Nav/Nav';
+import classes from './Toolbar.module.css';
 
 export default () => ( 
 <div className={classes.Toolbar}>
-    Toolbar
+ <Logo />
+ <Nav />
 </div>
 );
