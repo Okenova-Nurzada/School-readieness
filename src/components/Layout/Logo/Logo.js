934cbf623f79fe913dfacbfbda76e4e40c271db0
Nurzada <nura.okenova@gmail.com>
2020-04-19 13:18:38 +0600

    Added logo titlle

--- src/components/Layout/Logo/Logo.js
+++ src/components/Layout/Logo/Logo.js
@@ -1,8 +1,9 @@
 import React from 'react';
+
 import classes from './Logo.module.css';
 
 export default () => ( 
-<div className={classes.Logo}>
-    Logo
+<div className={classes.logo}>
+   <span>School-rediness</span>
 </div>
 );
--- src/components/StationeryBuilder/StationeryKit/Stationery/Stationery.js
+++ src/components/StationeryBuilder/StationeryKit/Stationery/Stationery.js
@@ -2,26 +2,8 @@ import React from 'react';
 import classes from './Stationery.module.css';
 
 export default ({ type }) => {
-    const stationeryClasses = [classes.Stationery]
-    switch (type) {
-        case 'pencil':
-            stationeryClasses.push(classes.pencil);
-            break;
-        case 'notebook':
-            stationeryClasses.push(classes.notebook);
-            break;
-        case 'pencil':
-            stationeryClasses.push(classes.pencil);
-            break;
-        case 'book':
-            stationeryClasses.push(classes.book);
-        default:
-            break;
-        case 'pencil':
-            stationeryClasses.push(classes.pencil);
-            break;
-    };
-
+    const stationeryClasses = [classes.Stationery, classes[type]]
+   
     return (
         <div className={stationeryClasses.join(' ')}>
         </div>
