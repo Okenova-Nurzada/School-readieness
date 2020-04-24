932bd9eaa369379f7ef3976313e61d615c0c679c
Nurzada <nurzadaokenova14@gmail.com>
2020-04-24 18:46:53 +0600

    Added Order button

--- src/components/StationeryBuilder/StationeryControls/StationeryControls.js
+++ src/components/StationeryBuilder/StationeryControls/StationeryControls.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import classes from './StationeryControls.module.css';
-import StationeryControl from './StationeryControl/StationeryControl';
+import React from "react";
+import classes from "./StationeryControls.module.css";
+import StationeryControl from "./StationeryControl/StationeryControl";
 const CONTROLS = [
-    { label: "Snack", type: "snack" },
-    { label: "Notebook", type: "notebook" },
-    { label: "Pen", type: "pen" },
-    { label: "Eraser", type: "eraser" },
-    { label: "Ruler", type: "ruler" },
-    { label: "Scissors", type: "scissors" },
+  { label: "Snack", type: "snack" },
+  { label: "Notebook", type: "notebook" },
+  { label: "Pen", type: "pen" },
+  { label: "Eraser", type: "eraser" },
+  { label: "Ruler", type: "ruler" },
+  { label: "Scissors", type: "scissors" },
 ];
-export default ({ items, addItems, removeItems }) => {
+export default ({ items, addItems, removeItems, canOrder }) => {
+  const controlsOutput = CONTROLS.map((control) => (
+    <StationeryControl
+      key={control.type}
+      control={control}
+      addItems={addItems}
+      removeItems={removeItems}
+      disabled={items[control.type] === 0}
+    />
+  ));
 
-    const controlsOutput = CONTROLS.map(control => <StationeryControl
-        key={control.type}
-        control={control}
-        addItems={addItems}
-        removeItems={removeItems}
-        disabled={items[control.type] === 0} />);
-
-    return (
-        <div className={classes.StationeryControls}>
-            {controlsOutput}
-            <button className={classes.orderButton}>ORDER</button>
-        </div>
-
-    );
-}
+  return (
+    <div className={classes.StationeryControls}>
+      {controlsOutput}
+      <button disabled={!canOrder} className={classes.orderButton}>
+        ORDER
+      </button>
+    </div>
+  );
+};
--- src/containers/StationeryBuilder/StationeryBuilder.js
+++ src/containers/StationeryBuilder/StationeryBuilder.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import StationeryKit from '../../components/StationeryBuilder/StationeryKit/StationeryKit';
-import StationeryControls from '../../components/StationeryBuilder/StationeryControls/StationeryControls';
-import classes from './StationeryBuilder.module.css';
+import React, { useState } from "react";
+import StationeryKit from "../../components/StationeryBuilder/StationeryKit/StationeryKit";
+import StationeryControls from "../../components/StationeryBuilder/StationeryControls/StationeryControls";
+import classes from "./StationeryBuilder.module.css";
 const PRICES = {
   notebook: 10,
   pen: 5,
@@ -9,10 +9,8 @@ const PRICES = {
   eraser: 20,
   ruler: 20,
   scissors: 25,
-
-}
+};
 export default () => {
-
   const [items, setItems] = useState({
     notebook: 0,
     pen: 0,
@@ -20,43 +18,48 @@ export default () => {
     eraser: 0,
     ruler: 0,
     scissors: 0,
-
   });
   const [price, setPrice] = useState(10);
+  const [canOrder, setCanOrder] = useState(false);
+
+  function checkCanOrder(items) {
+    const total = Object.keys(items).reduce((total, item) => {
+      return total + items[item];
+    }, 0);
+    setCanOrder(total > 0);
+  }
 
   function addItems(type) {
     const newItems = { ...items };
     newItems[type]++;
     setItems(newItems);
+    checkCanOrder(newItems);
 
     const newPrice = price + PRICES[type];
     setPrice(newPrice);
   }
+
   function removeItems(type) {
     if (items[type] >= 1) {
       const newItems = { ...items };
       newItems[type]--;
       setItems(newItems);
-
+      checkCanOrder(newItems);
 
       const newPrice = price - PRICES[type];
       setPrice(newPrice);
-    };
+    }
   }
-
-
-
-
 
   return (
     <div className={classes.StationeryBuilder}>
       <StationeryKit price={price} items={items} />
-      <StationeryControls items={items}
+      <StationeryControls
+        items={items}
+        canOrder={canOrder}
         addItems={addItems}
-        removeItems={removeItems} />
+        removeItems={removeItems}
+      />
     </div>
   );
 };
-
-
-
