937415e45e756cac84d275751385481733a6dd85
Nurzada <nurzadaokenova14@gmail.com>
2020-04-28 04:25:45 +0600

    Rename Nav component to Navigation

--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 import Logo from "../Logo/Logo";
-import Nav from './Nav/Nav';
-import classes from './Toolbar.module.css';
+import Navigation from "./Navigation/Navigation";
+import classes from "./Toolbar.module.css";
 
-export default () => ( 
-<div className={classes.Toolbar}>
- <Logo />
- <Nav />
-</div>
+export default () => (
+  <div className={classes.Toolbar}>
+    <Logo />
+    <nav>
+      <Navigation />
+    </nav>
+  </div>
 );
