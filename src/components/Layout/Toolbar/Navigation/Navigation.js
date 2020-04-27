eea6a9e978cb9798c9e1e729f2ad0536bb0ec2ea
Nurzada <nurzadaokenova14@gmail.com>
2020-04-28 04:50:34 +0600

    Created NavigationItem component

--- src/components/Layout/Toolbar/Navigation/Navigation.js
+++ src/components/Layout/Toolbar/Navigation/Navigation.js
@@ -1,3 +1,9 @@
 import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./Navigation.module.css";
-export default () => <nav className={classes.Navigation}>Navigation</nav>;
+export default () => (
+  <ul className={classes.Navigation}>
+    <NavigationItem>School bag</NavigationItem>
+    <NavigationItem>Checkout</NavigationItem>
+  </ul>
+);
