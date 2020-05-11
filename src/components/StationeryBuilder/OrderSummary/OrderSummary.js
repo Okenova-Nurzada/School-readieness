f4dfbdb6e6a2752920075966f7038ab53593daa6
Nurzada <nurzadaokenova14@gmail.com>
2020-05-11 08:36:28 +0600

    Fix react warnings and errors

--- src/components/StationeryBuilder/OrderSummary/OrderSummary.js
+++ src/components/StationeryBuilder/OrderSummary/OrderSummary.js
@@ -15,13 +15,13 @@ export default ({ items, cancelOrder, finishOrder }) => {
   const itemsOutput = Object.keys(items)
     .filter((item) => items[item] > 0)
     .map((item) => (
-      <li>
+      <li key={items}>
         {LABELS[item]}:{items[item]}
       </li>
     ));
   return (
     <div className={classes.OrderSummary}>
-      <h2>Your order!</h2>
+      <h2>Your order.</h2>
       <p>Congratulation! You've built a best stationery of all time!</p>
       <ul>{itemsOutput}</ul>
       <p>Would you like to checkbot?</p>
--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "../../../assets/logo.jpg";
+import logo from "../../../assets/Logo-1.jpg";
 import classes from "./Logo.module.css";
 
 export default () => (
   <div className={classes.logo}>
-    <img src={logo} />
+    <img src={logo} alt="School bag logo" />
     <span>School bag</span>
   </div>
 );
