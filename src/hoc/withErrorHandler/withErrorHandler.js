6b24f611c63473fe032c3ba5b70f884848652d82
Nurzada <nurzadaokenova14@gmail.com>
2020-05-25 05:29:14 +0600

    Fix withErrorHandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Modal from "../../components/UI/Modal/Modal";
-
 const withErrorHandler = (WrappedComponent, axios) => {
   return (props) => {
     const [error, setError] = useState(null);
-
     function hideModal() {
       setError(false);
     }
-
+    const requestInterceptor = axios.interceptors.request.use((request) => {
+      setError(false);
+      return request;
+    });
+    const responseInterceptor = axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        setError(error);
+        return Promise.reject(error);
+      }
+    );
     useEffect(() => {
-      const requestInterceptor = axios.interceptors.request.use((request) => {
-        setError(false);
-        return request;
-      });
-      const responseInterceptor = axios.interceptors.response.use(
-        (response) => response,
-        (error) => setError(error)
-      );
       return () => {
         axios.interceptors.request.eject(requestInterceptor);
         axios.interceptors.response.eject(responseInterceptor);
