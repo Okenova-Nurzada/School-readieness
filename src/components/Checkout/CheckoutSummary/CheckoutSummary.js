8ac2e722b980da5931a65856edb8f3d415fab3f1
Nurzada <nurzadaokenova14@gmail.com>
2020-05-25 06:55:12 +0600

    Add button event listeners

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -1,14 +1,18 @@
 import React from "react";
 import classes from "./CheckoutSummary.module.css";
 import StationeryKit from "../../StationeryBuilder/StationeryKit/StationeryKit";
-import Button from "../../UI/Button/Button"
+import Button from "../../UI/Button/Button";
 
-export default ({ price,items }) => {
+export default ({ price, items, checkoutCancel, checkoutContinue }) => {
   return (
     <div className={classes.CheckoutSummary}>
-      <StationeryKit  price={price} items= {items}/>
-      <Button green>Continue></Button>
-      <Button red>Cancel</Button>
+      <StationeryKit price={price} items={items} />
+      <Button click={checkoutCancel} red>
+        Cancel
+      </Button>
+      <Button click={checkoutContinue} green>
+        Continue
+      </Button>
     </div>
   );
 };
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import CheckoutSummary from "../../components/Checkout/CheckoutSummary/CheckoutSummary";
 import classes from "./Checkout.module.css";
 
 export default () => {
+  const history = useHistory();
   const items = {
     pen: 2,
     notebook: 3,
@@ -13,9 +15,22 @@ export default () => {
   };
   const price = 123;
 
+  function checkoutCancel() {
+    history.push("/builder");
+  }
+
+  function checkoutContinue() {
+    history.push("/checkout/finish");
+  }
+
   return (
     <div className={classes.Checkout}>
-      <CheckoutSummary items={items} price={price} />
+      <CheckoutSummary
+        items={items}
+        price={price}
+        checkoutCancel={checkoutCancel}
+        checkoutContinue={checkoutContinue}
+      />
     </div>
   );
 };
