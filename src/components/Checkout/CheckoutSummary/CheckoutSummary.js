44e3b5e8734b3f772e3f9be3b92e5dad7ff56299
Nurzada <nurzadaokenova14@gmail.com>
2020-05-25 08:34:05 +0600

    Hide buttons based on route

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -1,18 +1,21 @@
 import React from "react";
-import classes from "./CheckoutSummary.module.css";
+import { Route } from "react-router-dom";
 import StationeryKit from "../../StationeryBuilder/StationeryKit/StationeryKit";
 import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary.module.css";
 
 export default ({ price, items, checkoutCancel, checkoutContinue }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <StationeryKit price={price} items={items} />
-      <Button click={checkoutCancel} red>
-        Cancel
-      </Button>
-      <Button click={checkoutContinue} green>
-        Continue
-      </Button>
+      <Route path="/checkout" exact>
+        <Button click={checkoutCancel} red>
+          Cancel
+        </Button>
+        <Button click={checkoutContinue} green>
+          Continue
+        </Button>
+      </Route>
     </div>
   );
 };
