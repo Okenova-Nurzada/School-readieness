fc1af9935bf1ecbe26aa22e18f29b2984ed36dd0
Nurzada <nurzadaokenova14@gmail.com>
2020-05-25 08:01:17 +0600

    Add Checkoutform component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,29 +1,34 @@
-import React from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation, Route } from "react-router-dom";
 import CheckoutSummary from "../../components/Checkout/CheckoutSummary/CheckoutSummary";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import classes from "./Checkout.module.css";
 
 export default () => {
   const history = useHistory();
   const location = useLocation();
+  const [items, setItems] = useState({});
+  const [price, setPrice] = useState(0);
 
-  const query = new URLSearchParams(location.search);
-  let price = 0;
-  let items = {};
-  query.forEach((value, key) => {
-    if (key === "price") {
-      price = +value;
-    } else {
-      items[key] = +value;
-    }
-  });
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    const newItems = {};
+    query.forEach((value, key) => {
+      if (key === "price") {
+        setPrice(+value);
+      } else {
+        newItems[key] = +value;
+      }
+    });
+    setItems(newItems);
+  }, []);
 
   function checkoutCancel() {
     history.push("/builder");
   }
 
   function checkoutContinue() {
-    history.push("/checkout/finish");
+    history.push("/checkout/form");
   }
 
   return (
@@ -34,6 +39,9 @@ export default () => {
         checkoutCancel={checkoutCancel}
         checkoutContinue={checkoutContinue}
       />
+      <Route path="/checkout/form">
+        <CheckoutForm />
+      </Route>
     </div>
   );
 };
