98264d5df70c0f94d74cf11c9292991f71b95460
Nurzada <nurzadaokenova14@gmail.com>
2020-05-27 12:10:06 +0600

    Use dispatcher

--- src/components/StationeryBuilder/StationeryControls/StationeryControl/StationeryControl.js
+++ src/components/StationeryBuilder/StationeryControls/StationeryControl/StationeryControl.js
@@ -1,18 +1,27 @@
 import React from "react";
 import classes from "./StationeryControl.module.css";
+import { useDispatch } from "react-redux";
+import { ADD_ITEM, REMOVE_ITEM } from "../../../../store/actions";
 
-export default ({ control, addItems, removeItems, disabled }) => (
-  <div className={classes.StationeryControl}>
-    <button
-      className={classes.less}
-      onClick={() => removeItems(control.type)}
-      disabled={disabled}
-    >
-      -
-    </button>
-    <span className={classes.label}>{control.label}</span>
-    <button className={classes.more} onClick={() => addItems(control.type)}>
-      +
-    </button>
-  </div>
-);
+export default ({ control, removeItem, addItem, disabled }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={classes.StationeryControl}>
+      <button
+        className={classes.less}
+        onClick={() => dispatch({ type: REMOVE_ITEM })}
+        disabled={disabled}
+      >
+        -
+      </button>
+      <span className={classes.label}>{control.label}</span>
+      <button
+        className={classes.more}
+        onClick={() => dispatch({ type: ADD_ITEM })}
+      >
+        +
+      </button>
+    </div>
+  );
+};
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,9 +8,11 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case actions.ADD_ITEM:
+      console.log("item added");
       return state;
 
     case actions.REMOVE_ITEM:
+      console.log("item removed");
       return state;
 
     default:
